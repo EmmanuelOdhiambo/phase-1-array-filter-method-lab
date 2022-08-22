91fd48b99c663e7543afd75703657a160e2afcc0
Emmanuel Odhiambo <emmanuel3odhiambo@gmail.com>
2022-08-22 10:54:09 +0300

    array filter method complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+let drivers = ['Ocon', 'Vettel', 'Max', 'Leclerc', 'Lewis', 'Yuki'];
+
+function findMatching(drivers, name) {
+    return drivers = drivers.filter(driver => driver  === name || driver == name.toLowerCase())
+}
+
+
+function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.indexOf(query) == 0)
+
+}
+
+
+function matchName(drivers, name){
+    return drivers.filter(drivers => drivers.name === name);
+}
